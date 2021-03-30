0a9d16e134afbd5ce1e087676e569672af63af59
schweiny-08 <andrew.magri@outlook.com>
2021-03-30 23:48:04 +0200

    Created Investigator class
    Investigations list commented out until class is created

---
+++ Nemesys/Models/Investigator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Nemesys.Models
+{
+    public class Investigator : Reporter
+    {
+        /*public List<Investigations> investigations;*/
+
+        public Investigator() : base()
+        {
+            deptNum = 0;
+            /*investigations = new List<Investigations>;*/
+        }
+
+        public int deptNum { set; get; }
+    }
+}
