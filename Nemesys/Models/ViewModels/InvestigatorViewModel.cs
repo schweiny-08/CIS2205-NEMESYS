6211b0830160f3f955d64f448f2719e4ad79b04c
schweiny-08 <andrew.magri@outlook.com>
2021-05-01 02:33:03 +0200

    Added reporter and investigator form models

---
+++ Nemesys/Models/ViewModels/InvestigatorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Nemesys.Models.ViewModels
+{
+    public class InvestigatorViewModel
+    {
+        public int idNum { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public byte[] image { get; set; }
+        public int deptNum { set; get; }
+    }
+}
---
+++ Nemesys/Models/ViewModels/ReporterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Nemesys.Models.ViewModels
+{
+    public class ReporterViewModel
+    {
+        public int idNum { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public byte[] image { get; set; }
+    }
+}
