46cbc7c1eebfc9e8f820e5007c0dd1072d799b95
schweiny-08 <andrew.magri@outlook.com>
2021-06-06 03:35:26 +0200

    did hazard type controllers

--- Nemesys/Controllers/HazardTypesController.cs
+++ Nemesys/Controllers/HazardTypesController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Nemesys.DAL;
 using Nemesys.Models;
 using Nemesys.Models.Interfaces;
@@ -15,70 +17,111 @@ namespace Nemesys.Controllers
     public class HazardTypesController : Controller
     {
         private readonly INemesysRepository _nemesysRepository;
+        private readonly ILogger<HazardTypesController> _logger;
 
-        public HazardTypesController(INemesysRepository nemesysRepository)
+        public HazardTypesController(INemesysRepository nemesysRepository, ILogger<HazardTypesController> logger)
         {
             _nemesysRepository = nemesysRepository;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            var model = new HazardTypeListViewModel()
+            try
             {
-                TotalEntries = _nemesysRepository.GetAllHazardTypes().Count(),
-                HazardTypes = _nemesysRepository.GetAllHazardTypes().Select(h => new HazardTypeViewModel { 
-                    Id = h.Id,
-                    hazardTypeName = h.hazardTypeName
-                })
-            };
+                var model = new HazardTypeListViewModel()
+                {
+                    TotalEntries = _nemesysRepository.GetAllHazardTypes().Count(),
+                    HazardTypes = _nemesysRepository.GetAllHazardTypes().Select(h => new HazardTypeViewModel
+                    {
+                        Id = h.Id,
+                        hazardTypeName = h.hazardTypeName
+                    })
+                };
 
-            return View(model);
+                return View(model);
+            }
+            catch (Exception e) {
+                _logger.LogError(e, e.Message, e.Data);
+                return View("Error");
+            }
         }
 
         [HttpGet]
         public IActionResult Details(int id)
         {
-            var hazardType = _nemesysRepository.GetHazardTypeById(id);
+            try
+            {
+                var hazardType = _nemesysRepository.GetHazardTypeById(id);
 
-            if (hazardType == null)
-                return NotFound();
-            else {
-                var model = new HazardTypeViewModel()
+                if (hazardType == null)
+                    return NotFound();
+                else
                 {
-                    Id = hazardType.Id,
-                    hazardTypeName = hazardType.hazardTypeName
-                };
-                return View(model);
+                    var model = new HazardTypeViewModel()
+                    {
+                        Id = hazardType.Id,
+                        hazardTypeName = hazardType.hazardTypeName
+                    };
+                    return View(model);
+                }
+            }
+            catch (Exception e) {
+                _logger.LogError(e, e.Message, e.Data);
+                return View("Error");
             }
         }
 
        [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception e) {
+                _logger.LogError(e, e.Message, e.Data);
+                return View("Error");
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create([Bind("hazardTypeName")] HazardType newHazardType)
         {
-            if (ModelState.IsValid)
+            try
             {
-                HazardType hazardType = new HazardType()
+                if (ModelState.IsValid)
                 {
-                    hazardTypeName = newHazardType.hazardTypeName
-                };
+                    HazardType hazardType = new HazardType()
+                    {
+                        hazardTypeName = newHazardType.hazardTypeName
+                    };
 
-                _nemesysRepository.AddNewHazardType(hazardType);
+                    _nemesysRepository.AddNewHazardType(hazardType);
 
-                return RedirectToAction("Index");
+                    return RedirectToAction("Index");
+                }
+                else
+                    return View(newHazardType);
             }
-            else
+            catch (DbUpdateException e)
+            {
+                SqlException s = e.InnerException as SqlException;
+                if (s != null)
+                {
+                    ModelState.AddModelError(string.Empty, "A database error occured - please contact an admin.");
+                }
                 return View(newHazardType);
+            }
+            catch (Exception e) {
+                _logger.LogError(e, e.Message, e.Data);
+                return View("Error");
+            }
         }
 
-        // Since hazard types dont have a lot of data associated with it, 
+        // Since hazard types dont have a lot of data associated with it, no need to edit
 
 /*
       [HttpGet]
@@ -136,15 +179,22 @@ namespace Nemesys.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
         {
-            var hazardType = _nemesysRepository.GetHazardTypeById(id);
-            if (hazardType == null)
-                return NotFound();
-            if (hazardType.Reports != null)
-                _nemesysRepository.DeleteHazardType(hazardType);
-            else
-                return BadRequest("This hazard type has reports connected to it!");
+            try
+            {
+                var hazardType = _nemesysRepository.GetHazardTypeById(id);
+                if (hazardType == null)
+                    return NotFound();
+                if (hazardType.Reports != null)
+                    _nemesysRepository.DeleteHazardType(hazardType);
+                else
+                    return BadRequest("This hazard type has reports connected to it!");
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (Exception e) {
+                _logger.LogError(e, e.Message, e.Data);
+                return View("Error");
+            }
         }
     }
 }
